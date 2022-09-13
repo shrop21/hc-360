8895299b0f0cc42f55ca5ad7407763877619e2ec
jake.shropshire <jake.shropshire@teladoc.com>
2022-09-13 09:03:23 -0500

    Removing logo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import MedicationManager from './pages/medication-manager';
 function App() {
   return (
     <div>
-      <header className="app-header">
-        <img src="https://s3.amazonaws.com/images.teladoc.com/member/Logo-Horz-RGB.svg" alt="teladoc-logo" height='40px' />
-      </header>
       {/* <AppStore /> */}
       <MedicationManager />
     </div>
